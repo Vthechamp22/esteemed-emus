363e18c469fb27e043d6a290ce12342f8361fbf1
Vivaan Parashar <parashar.xtra@gmail.com>
2021-07-14 22:31:33 +0530

    Add docstrings and typehint functions.py

--- myoure/functions.py
+++ myoure/functions.py
@@ -1,13 +1,23 @@
 from pathlib import Path
+from typing import List, NoReturn
 
 
-def list_content(path):
+def list_content(path: str) -> List:
+    """Lists the files and directories in the given path.
+
+    :param path: The path to list folders and files from.
+    """
     entries = Path(path)
     content_list = [i.name for i in entries.iterdir()]
     return content_list
 
 
-def mkdir(path, name=None):
+def mkdir(path: str, name: str = None) -> NoReturn:
+    """Makes a directory in the given path with the given name
+
+    :param path: The path to the parent directory of the new folder
+    :param name: The name of the new folder, None by default.
+    """
     index = 0
     while True:
         try:
@@ -18,7 +28,11 @@ def mkdir(path, name=None):
             index += 1
 
 
-def rmdir(path):
+def rmdir(path: str) -> NoReturn:
+    """Removes the given directory
+
+    :param path: Path to the directory
+    """
     path = Path(path)
     try:
         path.rmdir()
